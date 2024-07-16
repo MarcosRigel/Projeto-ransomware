9246772cca2af5ce4258a54bf68232833e58f149
MarcosRigel <marcosrigel99@gmail.com>
2024-07-16 20:38:36 -0300

    Projeto finalizado

--- decrypter.py
+++ decrypter.py
@@ -3,7 +3,7 @@ import pyaes
 
 ## abrir o arquivo criptografado
 
-file_name = 'texte.txt.ransomwaretroll'
+file_name = 'text.txt.ransomwaretroll'
 file = open(file_name, 'rb')
 file_data = file.read()
 file.close()
@@ -19,11 +19,7 @@ os.remove(file_name)
 
 ## criar um novo arquivo descriptografado
 
-new_file = 'teste.txt'
+new_file = 'text.txt'
 new_file = open(f'{new_file}','wb')
 new_file;write(decrypt_data)
 new_file.close()
-
-
-
-
